370be81306de4930ecd52a31cce1c5a5f988f671
Marcus Lo <marcusklo1010@gmail.com>
2024-02-22 10:05:36 -0700

    javascript functions continued complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity) {
+    if (!activity) {
+      return "This Saturday, I want to roller-skate!";
+    } else {
+      return `This Saturday, I want to ${activity}!`;
+    }
+  }
+
+  function mondayWork(work) {
+    if (!work) {
+      return "This Monday, I will go to the office.";
+    } else {
+      return `This Monday, I will ${work}.`;
+    }
+  }
+
+  function wrapAdjective(highlight) {
+    return function(adjective) {
+      return `You are ${highlight}${adjective}${highlight}!`;
+    };
+  }
